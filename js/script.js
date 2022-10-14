e47ef4ed2388fc31570bdb3aa2b70b182ebd9f34
ValentinaZussino <vzussino@protonmail.com>
2022-10-14 18:21:39 +0200

    esercizio 2

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const verifica = function() {
     } else {
     document.querySelector('p').innerHTML = 'la tua parola NON &egrave; palindroma';
     }
+    parolaInput.value = '';
 }
 // on click
 btnVerifica.addEventListener('click', verifica);
@@ -28,3 +29,41 @@ btnVerifica.addEventListener('click', verifica);
 // Dichiariamo chi ha vinto.
 
 // ES GIOCO PARI E DISPARI
+// faccio scegliere all'utente se pari o dispari 
+const sceltaUtInput = document.getElementById('scelta-utente');
+// creo valori obbligatori
+const pari = 'pari';
+const dispari = 'dispari';
+// faccio scegliere all'utente numero
+const numeroUtInput = document.getElementById('numero-utente');
+// prendo btn gioca
+const btnGioca = document.getElementById('btn-gioca');
+
+// on click
+btnGioca.addEventListener('click', function(){
+    // inizio a impedire di mettere parole diverse e numeri > di 5
+    let sceltaUtValue = sceltaUtInput.value;
+    if(sceltaUtValue != pari && sceltaUtValue != dispari){
+        alert('puoi scrivere solo o pari o dispari');
+    }
+    let numeroUtValue = parseInt(numeroUtInput.value);
+    if((numeroUtValue < 0 || numeroUtValue > 5)){
+    alert('metti un numero da 1 a 5');
+    }
+    // genero num random del pc
+    let numPc = randomNumber(1, 5);
+    console.log(numPc);
+    // somma num ut + num pc
+    let somma = numPc + numeroUtValue;
+    console.log(somma);
+    // verifica se pari o disp con iseven e confronto con valore inserito
+    // let verifica = isEven(somma);
+    // console.log(verifica);
+    if(isEven(somma) && sceltaUtValue == pari){
+        console.log('ha vinto l\'utente');
+    } else if(!isEven(somma) && sceltaUtValue == dispari){
+        console.log('ha vinto l\'utente');
+    } else {
+        console.log('ha vinto il pc');
+    }
+}) 
