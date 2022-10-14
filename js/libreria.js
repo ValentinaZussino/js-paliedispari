fc1b6b3d40f5b16ec4ca030fc4b47a7fdce17f08
ValentinaZussino <vzussino@protonmail.com>
2022-10-14 14:59:10 +0200

    creaz libreria + verif funzione palindrome ok

---
+++ js/libreria.js
@@ -0,0 +1,19 @@
+// NUMERI RANDOM
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// PARI DISPARI
+function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+// PALINDROMO
+function palindrome(str) {
+    var lowStr = str.toLowerCase();
+    var reverseStr = lowStr.split('').reverse().join(''); 
+    return reverseStr === lowStr;
+}
